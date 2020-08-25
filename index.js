78bdbce54bad574898d89f395420e295beb9178f
ThanLN <nhamthanuit@gmail.com>
2020-08-25 17:01:34 +0700

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ console.log('Sent list of items');
 });
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
+app.get('/', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
